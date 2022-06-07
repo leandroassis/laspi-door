fc5f806493abf0d7be05e4495bbec6cd46397f19
Leandro Assis <assissantosleandro@poli.ufrj.br>
2022-06-07 18:45:24 -0300

    Atualização usando websockets

--- main/main.ino
+++ main/main.ino
@@ -1,13 +1,12 @@
 #include <SPI.h>
 #include <MFRC522.h>
-#include <SD.h>
 #include <Ethernet.h>
-#include <CSV_Parser.h>
+#include <EEPROM.h>
+#include <WebSocketsClient.h>
  
 #define RFID_SS_PIN      7          // Chip Enable do sensor rfid    
-#define SD_SS_PIN        4          // Chip´enable do cartão sd
 #define ETHERNET_SS_PIN  10         // Chip enable do módulo ethernet
-#define RST_PIN     9           // Reset do sensor rfid
+#define RST_PIN     9           // Reset do barramento SPI
 #define RELAY_PIN   4           // Pino de acionar a relé
 #define RELAY2     A0           // Pino de apoio ao acionamento da relé 
 #define INTERRUP    3           // Pino de conexão com o clock do 555
@@ -16,33 +15,34 @@
 #define BLUE       A4           // Pino de controle do led de indicação
 #define GREEN      A3           // Pino de controle do led de indicação
 
-#define TAG_COUNT 160            // Quantidade de cartões a serem implementados(MAX: 230)
+#define TAG_COUNT 50            // Quantidade de cartões a serem implementados(MAX: 230)
  
-//MFRC522 rfid(SS_PIN, RST_PIN);  
-EthernetServer server(80);
-CSV_Parser cp("ss", true, ';'); // Inicializa o parser 
+MFRC522 rfid(RFID_SS_PIN, RST_PIN);
+WebSocketsClient webSocket;
 
 byte mac_address[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
-byte ip_address[] = {192, 168, 88, 19};
-byte gateway[] = {192, 168, 88, 1};
+byte ip_address[] = {192, 168, 1, 15}; // 192.168.88.19
+byte gateway[] = {192, 168, 1, 254};
 byte subnet[] = {255, 255, 255, 0};
 
-String *tags_temp, *ids_temp;
-String readString;
-
-File tags_database;
+String tags_temp[TAG_COUNT];
+int free_address;
 
 void setup(){
   
   Serial.begin(9600);           
   Serial.println("Inicializando o sistema");
-  SPI.begin();                 // Inicia a comunicação SPI
-  //rfid.PCD_Init();             // Inicia o sensor RFID
-  Ethernet.begin(mac_address, ip_address, gateway, subnet);
-  server.begin();
-  SD.begin(SD_SS_PIN);         // Inicia o leitor de cartão microSD
-  // Caso o SD conflite no barramento SPI, ele deve ser desligado e ligado apenas quando o SD for ser utilizado
-  Serial.println("Ethernet, SD, e barramento SPI configurados e setados");
+
+  SPI.begin();                 // Inicia a comunicação SPI 
+  rfid.PCD_Init();             // Inicia o sensor RFID
+  Ethernet.begin(mac_address, ip_address, gateway, subnet); // Inicia o shield Ethernet
+
+  Serial.println("Ethernet, sensor RFID, e barramento SPI configurados e setados.");
+  delay(1500);
+
+  webSocket.begin("192.168.88.14", 8011);
+  webSocket.onEvent(handleWebSocketEvent);
+  Serial.println("Websocket configurado e conectado com sucesso.");
   
   pinMode(RELAY_PIN, OUTPUT);
   pinMode(KEEP_PIN, OUTPUT);
@@ -67,33 +67,28 @@ void setup(){
 
   Serial.println("Pinos do arduino inicializados com sucesso.");
 
-  tags_database = SD.open("database.csv", FILE_WRITE);
+  EEPROMDump();
+  Serial.println("Tags dumpadas da memória para o vetor temporário.");
 
-  if(tags_database && tags_database.size() == 0){
-    tags_database.write("uid;nome\n");
-    tags_database.close();
-  }
-  
-  DatabaseDump();
-
-  Serial.println("Tags dumpados para os vetores temporários");
+  /*
+  Linha de código rodada para inicializar pela primeira vez a eeprom do arduino
+  EEPROM.write(0, 1);
+  for(int i = 1; i < 1024; i++) EEPROM.write(i, 0xFF);
+  */
 }
  
 void loop() {
   bool RFID_Found = false;                // Variável auxiliar para controle de interface do usuário
   String strID = "";                      // Variável para guardar o uid lido
-
-  EthernetClient client = server.available();
-  ClientAdmin(client); 
   
-  /*
   if(!rfid.PCD_PerformSelfTest()){
     // Performa selftest no sensor RFID, caso falhe a porta é mantida aberta
     PermanentStateError();
   }
   
+  webSocket.loop();
+  
   strID = read_UID();
-  */
 
   for(int i = 0; i < TAG_COUNT; i++){
     if(strID == tags_temp[i]){ // Se a tag estiver no vetor de tags (foi cadastrada previamente) a porta abre
@@ -104,11 +99,10 @@ void loop() {
   }
   if(!RFID_Found) RFID_Rejected();
   
-  //rfid.PICC_HaltA();
-  //rfid.PCD_StopCrypto1();
+  rfid.PICC_HaltA();
+  rfid.PCD_StopCrypto1();
 }
 
-/*
 String read_UID(){
   String uid = "";
 
@@ -121,39 +115,103 @@ String read_UID(){
   
   return uid;
 }
-*/
 
-void ClientAdmin(EthernetClient client){
-  if(client){
-    Serial.println("Client web OK");
-    while(client.connected() && client.available()){
-      char c = client.read();
-      Serial.println(c);
-      if (readString.length() < 100) readString += c;  
-      if (c == '\n') {
-        if(readString.indexOf("Nome=")){
-          String nome = readString.substring(readString.indexOf("Nome=")+4, readString.indexOf("&"));
-          Serial.println(nome);
-          if(readString.indexOf("cadastrar=") > 0) writeTagInDatabase(nome);
-          else if(readString.indexOf("deletar=")) removeTagInDatabase(nome);
-        }
+void handleWebSocketEvent(WStype_t type, uint8_t *payload, size_t length){
+    switch(type){
+      case WStype_DISCONNECTED:
+        Serial.println("Server desconectado.");
+        break;
+      case WStype_CONNECTED:
+        Serial.println("Server conectado.");
+        webSocket.sendTXT("Okay!");
+        break;
+      case WStype_TEXT:
+        Serial.println("Recebido: "+String((char *)payload));
+        if(strcmp(payload, "cadastrar")){
+          // Le UID, cadastrar na eeprom e envia via TXT
+
+          digitalWrite(RED, LOW);
+          digitalWrite(GREEN, LOW);
+          for(int i = 0; i < 10; i++){
+            digitalWrite(BLUE, !digitalRead(BLUE));
+            delay(300);
+          }
         
-        client.println("HTTP/1.1 200 OK");
-        client.println("Content-Type: text/html");
-        client.println("<!DOCTYPE HTML><html><head>"); 
-        client.println("<link rel='icon' type='image/png' href='https://zenprospect-production.s3.amazonaws.com/uploads/pictures/605c0a99e4a118000171b6b7/picture'/>");
-        client.println("<title>Portal WEB do Controle de Acesso</title></head><body style='background-color:#f0f3f5'>");
-        client.println("<br/><center><font color='#1262c0'><h1>Controle de Acesso - LASPI</h1></font></center>");
-        client.println("<br/><center><form method='get'><label for='nome-input'>Nome associado ao cartão:</label><br><input id='nome-input' name='Nome' placeholder='Nome ou identificação'/><br><br><input type='submit' name='cadastrar' value='Cadastrar'/><input type='submit' name='deletar' value='Deletar'/></form></center>");
-        client.println("<br/><br/><center><font color='#1262c0'><h2>Cartões cadastrados atualmente:</h2></font></center><hr/>");
-        client.println("<center><table><tr><td><h3>Nome</h3></td><td><h3>UID</h3></td></tr>");
-        for(int i = 0; i < cp.getRowsCount(); i++) client.println("<tr><td><h3>"+ids_temp[i]+"</h3></td><td><h3>"+tags_temp[i]+"</h3></td></tr>");
-        client.println("</table></center><hr/><br/></body></html>");
-        readString="";
-        client.stop();
-      }
+          String tag = read_UID();
+          writeTagInEEPROM(tag);
+          EEPROMDump();
+          webSocket.sendTXT(tag);
+
+          digitalWrite(BLUE, LOW);
+          digitalWrite(GREEN, HIGH);
+          digitalWrite(RED, LOW);
+          delay(1000);
+          digitalWrite(BLUE, HIGH);
+          digitalWrite(GREEN, LOW);
+          digitalWrite(RED, LOW);
+
+          for(int i = 0; i < TAG_COUNT; i++) Serial.println(tags_temp[i]);
+        }
+        else if(strcmp(payload, "excluir")){
+          // seta o free_address para 1 e limpa toda a memória eeprom
+          EEPROM.write(0, 1);
+          for(int i = 1; i < 1024; i++) EEPROM.write(i, 0xFF);
+          webSocket.sendTXT("Limpo");
+        }
+        else{
+          // coloca a tag recebida na eeprom
+          writeTagInEEPROM(String((char *) payload));
+          webSocket.sendTXT("Ok");
+        }
     }
+}
+
+void EEPROMDump(){
+  // le todos os 1024 endereços da eeprom, faz o parser (monta uma tag a cada 4 bytes lidos) e copia para o vetor tags_temp
+  int contador = 0;
+  String temp = "";
+  
+  for(int j = 0; j < TAG_COUNT; j++) tags_temp[j] = "";
+  Serial.println("Vetor de tags limpo.");
+  
+  for(int i = 1; i <= 4*TAG_COUNT; i++){ // Dumpa a EEPROM em um array para evitar consultas excessivas    
+    contador++;
+    temp += String(EEPROM.read(i), HEX);
+    if(contador == 4){
+      contador = 0;
+      temp.toUpperCase();
+      tags_temp[i/4 - 1] = temp;
+      Serial.print(temp);
+      Serial.print("  ");
+      Serial.print(i);
+      Serial.print("\n");
+      temp = "";
+    }  
   }
+
+  Serial.println("Tags dumpadas com sucesso.");
+}
+
+void writeTagInEEPROM(String ID){
+    // Função para parsear a String ID em 4 bytes e escreve-os em endereços consecutivos na EEPROM
+    char temp_char[2];
+    free_address = EEPROM.read(0);
+    
+    Serial.println(ID);
+
+    for(int i = 0; i < TAG_COUNT; i++){
+      if(ID == tags_temp[i]) return;
+    }
+   
+    for(int i = 0; i < 8; i+=2){
+      temp_char[0] = ID[i];
+      temp_char[1] = ID[i+1];
+      EEPROM.write(free_address, strtoul(temp_char, NULL, 16));
+      free_address++;
+    }
+
+    // Após parsear e escrever a tag na EEPROM, salva no endereço 0 o próximo endereço livre
+    EEPROM.write(0, free_address);
 }
 
 void RFID_Rejected(){
@@ -188,72 +246,9 @@ void RFID_Accepted(){
   digitalWrite(BLUE, HIGH);
 }
 
-void removeTagInDatabase(String nome){
-  DatabaseDump();
-  SD.remove("database.csv");
-  tags_database = SD.open("database.csv", FILE_WRITE);
-  for(int i = 0; i < cp.getRowsCount(); i++){
-    if(ids_temp[i] != nome) tags_database.write(String(tags_temp[i]+";"+ids_temp[i]+"\n").c_str());
-  }
-  tags_database.close();
-}
-
-void writeTagInDatabase(String nome){
-    // Função para escrever a string ID no database.csv e depois atualizar o database externo
-
-    String tag = "";
-    
-    digitalWrite(RED, LOW);
-    digitalWrite(GREEN, LOW);
-    digitalWrite(BLUE, HIGH);
-    for(int i = 0; i < 10; i++){
-      digitalWrite(BLUE, !digitalRead(BLUE));
-      delay(200);
-    }
-
-    if(cp.getRowsCount() == TAG_COUNT+1){
-      ErrorAddingTag();
-      return;
-    }
-
-    //tag = read_UID();
-    for(int i = 0; i < cp.getRowsCount(); i++){
-      if(tag == tags_temp[i]){
-        ErrorAddingTag();
-        return;
-      }
-    }
-
-    tags_database = SD.open("database.csv", FILE_WRITE);
-
-    if(!tags_database){
-      ErrorAddingTag();
-      return;
-    }
-
-    if(!tags_database.write(String(tag+";"+nome+"\n").c_str())){
-      ErrorAddingTag();
-      return;
-    }
-    
-    tags_database.close();
-    
-    // Após escrever a tag no database.csv, dumpa tudo pro tag_temp 
-    DatabaseDump();
-
-    digitalWrite(BLUE, LOW);
-    digitalWrite(GREEN, HIGH);
-    digitalWrite(RED, LOW);
-    delay(1500);
-    digitalWrite(BLUE, HIGH);
-    digitalWrite(GREEN, LOW);
-    digitalWrite(RED, LOW);
-
-    return;
-}
-
 void ErrorAddingTag(){
   // Pisca 10 vezes em vermelho com intervalos de 0.2s e depois volta ao azul sinalizando erro ao cadastrar
+  Serial.println("Erro adicionando uma tag no database.");
   for(int i = 0; i < 10; i++){
         digitalWrite(BLUE, LOW);
         digitalWrite(GREEN, LOW);
@@ -270,6 +265,7 @@ void ErrorAddingTag(){
 
 void PermanentStateError(){
   // Led RGB fica piscando em vermelho com intervalos de 1.2s e abre a porta
+  Serial.println("Entrando em estado de erro permanente.");
   digitalWrite(BLUE, LOW);
   digitalWrite(GREEN, LOW);
   while(1){
@@ -280,12 +276,6 @@ void PermanentStateError(){
     digitalWrite(RED, LOW);
     delay(1200);
   }  
-}
-
-void DatabaseDump(){
-  cp.readSDfile("database.csv");
-  tags_temp = (String *) cp["uid"];
-  ids_temp = (String *) cp["nome"];
 }
 
 void KeepRelayAlive(){
